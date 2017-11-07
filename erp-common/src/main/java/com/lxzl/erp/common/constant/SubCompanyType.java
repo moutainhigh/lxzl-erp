38496228a5dbbe7a3251637f7f39c3242d738f52
liuke <liuke8080@163.com>
2017-11-07 15:48:58 +0800

    子公司类型枚举类

---
+++ erp-common/src/main/java/com/lxzl/erp/common/constant/SubCompanyType.java
@@ -0,0 +1,10 @@
+package com.lxzl.erp.common.constant;
+
+/**
+ * 公司类型
+ */
+public class SubCompanyType {
+    public static final Integer SUB_COMPANY_TYPE_HEADER = 1;//  1-总公司
+    public static final Integer SUB_COMPANY_TYPE_SUB = 2;// 2-分公司
+    public static final Integer SUB_COMPANY_TYPE_SUPPLIER = 3;  //3-供应商
+}
