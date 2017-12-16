eeb778b8906a0cb44a93373e89e26a02b82c34c6
gaochao <26127166@qq.com>
2017-12-16 10:25:23 +0800

    解决订单查询不成功问题
    Signed-off-by: gaochao <26127166@qq.com>

--- erp-common/src/main/java/com/lxzl/erp/common/domain/product/pojo/ProductMaterial.java
+++ erp-common/src/main/java/com/lxzl/erp/common/domain/product/pojo/ProductMaterial.java
@@ -18,10 +18,6 @@ public class ProductMaterial implements Serializable {
 	private Integer materialCount;   //物料总数
 	private Integer dataStatus;   //状态：0不可用；1可用；2删除
 	private String remark;   //备注
-	private Date createTime;   //添加时间
-	private String createUser;   //添加人
-	private Date updateTime;   //修改时间
-	private String updateUser;   //修改人
 
 	private String materialNo;
 	private String materialName;
@@ -82,38 +78,6 @@ public class ProductMaterial implements Serializable {
 
 	public void setRemark(String remark){
 		this.remark = remark;
-	}
-
-	public Date getCreateTime(){
-		return createTime;
-	}
-
-	public void setCreateTime(Date createTime){
-		this.createTime = createTime;
-	}
-
-	public String getCreateUser(){
-		return createUser;
-	}
-
-	public void setCreateUser(String createUser){
-		this.createUser = createUser;
-	}
-
-	public Date getUpdateTime(){
-		return updateTime;
-	}
-
-	public void setUpdateTime(Date updateTime){
-		this.updateTime = updateTime;
-	}
-
-	public String getUpdateUser(){
-		return updateUser;
-	}
-
-	public void setUpdateUser(String updateUser){
-		this.updateUser = updateUser;
 	}
 
 	public String getMaterialNo() {
