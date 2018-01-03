4c5f9728841ae9d219aeb3bfbd6a166a13edc8c2
xiaoluyu <xly19930523@163.com>
2018-01-03 21:46:03 +0800

    修改
    Signed-off-by: xiaoluyu <xly19930523@163.com>

---
+++ erp-common/src/main/java/com/lxzl/erp/common/domain/assembleOder/AssembleOrderQueryParam.java
@@ -0,0 +1,34 @@
+package com.lxzl.erp.common.domain.assembleOder;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.lxzl.erp.common.domain.base.BasePageParam;
+
+import java.util.Date;
+
+/**
+ * User : XiaoLuYu
+ * Date : Created in ${Date}
+ * Time : Created in ${Time}
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AssembleOrderQueryParam extends BasePageParam {
+
+    private Date createStartTime;
+    private Date createEndTime;
+
+    public Date getCreateStartTime() {
+        return createStartTime;
+    }
+
+    public void setCreateStartTime(Date createStartTime) {
+        this.createStartTime = createStartTime;
+    }
+
+    public Date getCreateEndTime() {
+        return createEndTime;
+    }
+
+    public void setCreateEndTime(Date createEndTime) {
+        this.createEndTime = createEndTime;
+    }
+}
