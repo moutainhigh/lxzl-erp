a460039ec1ed46604bfa34f4c9e76ecbd6fed65c
gaochao <26127166@qq.com>
2018-01-09 22:17:58 +0800

    客户导入系统程序
    Signed-off-by: gaochao <26127166@qq.com>

---
+++ erp-web/src/test/java/com/lxzl/erp/web/init/CustomerDataImport.java
@@ -0,0 +1,19 @@
+package com.lxzl.erp.web.init;
+
+import com.lxzl.erp.ERPUnTransactionalTest;
+import org.junit.Test;
+
+/**
+ * 描述: ${DESCRIPTION}
+ *
+ * @author gaochao
+ * @date 2018-01-07 17:37
+ */
+public class CustomerDataImport extends ERPUnTransactionalTest {
+
+    @Test
+    public void testImportCustomer() {
+
+    }
+
+}
