82a526db8f925667cbfe24386894056132a78201
Peng Bin Jie <603742275@qq.com>
2018-01-11 21:08:03 +0800

    转移单新增判断类型

---
+++ erp-common/src/main/java/com/lxzl/erp/common/domain/validGroup/TransferOrder/UpdateTransferOrderOutGroup.java
@@ -0,0 +1,10 @@
+package com.lxzl.erp.common.domain.validGroup.TransferOrder;
+
+/**
+ * @Author: your name
+ * @Description：
+ * @Date: Created in 20:04 2018/1/11
+ * @Modified By:
+ */
+public interface UpdateTransferOrderOutGroup {
+}
