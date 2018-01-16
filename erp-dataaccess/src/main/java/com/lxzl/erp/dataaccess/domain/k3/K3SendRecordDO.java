3967eb33d95e5f518256265565c04582611ef961
gaochao <26127166@qq.com>
2018-01-16 11:49:59 +0800

    补充提交K3
    Signed-off-by: gaochao <26127166@qq.com>

---
+++ erp-dataaccess/src/main/java/com/lxzl/erp/dataaccess/domain/k3/K3SendRecordDO.java
@@ -0,0 +1,64 @@
+package com.lxzl.erp.dataaccess.domain.k3;
+
+import com.lxzl.se.dataaccess.mysql.domain.BaseDO;
+import java.util.Date;
+
+
+public class K3SendRecordDO  extends BaseDO {
+
+	private Integer id;
+	private Integer recordType;
+	private String recordJson;
+	private Integer sendResult;
+	private Integer receiveResult;
+	private Date sendTime;
+
+	public Integer getId(){
+		return id;
+	}
+
+	public void setId(Integer id){
+		this.id = id;
+	}
+
+	public Integer getRecordType(){
+		return recordType;
+	}
+
+	public void setRecordType(Integer recordType){
+		this.recordType = recordType;
+	}
+
+	public String getRecordJson(){
+		return recordJson;
+	}
+
+	public void setRecordJson(String recordJson){
+		this.recordJson = recordJson;
+	}
+
+	public Integer getSendResult(){
+		return sendResult;
+	}
+
+	public void setSendResult(Integer sendResult){
+		this.sendResult = sendResult;
+	}
+
+	public Integer getReceiveResult(){
+		return receiveResult;
+	}
+
+	public void setReceiveResult(Integer receiveResult){
+		this.receiveResult = receiveResult;
+	}
+
+	public Date getSendTime(){
+		return sendTime;
+	}
+
+	public void setSendTime(Date sendTime){
+		this.sendTime = sendTime;
+	}
+
+}
