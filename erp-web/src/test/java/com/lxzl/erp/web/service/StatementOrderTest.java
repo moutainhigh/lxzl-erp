577f6557349007d7334b6d93b88fe3d3aa11287d
gaochao <26127166@qq.com>
2018-02-06 15:21:30 +0800

    修复结算单逾期问题。一旦有逾期，长租订单将不允许下单
    Signed-off-by: gaochao <26127166@qq.com>

--- erp-web/src/test/java/com/lxzl/erp/web/service/StatementOrderTest.java
+++ erp-web/src/test/java/com/lxzl/erp/web/service/StatementOrderTest.java
@@ -1,11 +1,15 @@
 package com.lxzl.erp.web.service;
 
 import com.lxzl.erp.core.service.statement.StatementService;
+import com.lxzl.erp.core.service.statement.impl.support.StatementOrderSupport;
+import com.lxzl.erp.dataaccess.domain.statement.StatementOrderDO;
 import com.lxzl.se.unit.test.BaseUnTransactionalTest;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 /**
  * 描述: ${DESCRIPTION}
@@ -18,9 +22,24 @@ public class StatementOrderTest  extends BaseUnTransactionalTest {
     @Autowired
     private StatementService statementService;
 
+    @Autowired
+    private StatementOrderSupport statementOrderSupport;
+
     @Test
     public void handleNoPaidStatementOrder(){
         Date currentTime = new Date();
         statementService.handleOverdueStatementOrder(null,currentTime);
+    }
+
+    @Test
+    public void testGetShortRentReceivable(){
+        BigDecimal totalShortRentReceivable = statementOrderSupport.getShortRentReceivable(731449);
+        System.out.println(totalShortRentReceivable);
+    }
+
+    @Test
+    public void getOverdueStatementOrderList(){
+        List<StatementOrderDO> statementOrderDOList = statementOrderSupport.getOverdueStatementOrderList(731477);
+        System.out.println(statementOrderDOList.size());
     }
 }
