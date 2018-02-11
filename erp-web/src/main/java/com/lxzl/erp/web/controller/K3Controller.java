c42c67fa0a2a4da9a0fa5f861bdaefe5cb966fde
gaochao <26127166@qq.com>
2018-02-11 09:54:01 +0800

    提供K3订单接口
    Signed-off-by: gaochao <26127166@qq.com>

--- erp-web/src/main/java/com/lxzl/erp/web/controller/K3Controller.java
+++ erp-web/src/main/java/com/lxzl/erp/web/controller/K3Controller.java
@@ -1,7 +1,10 @@
 package com.lxzl.erp.web.controller;
 
+import com.lxzl.erp.common.constant.ErrorCode;
+import com.lxzl.erp.common.domain.Page;
 import com.lxzl.erp.common.domain.ServiceResult;
 import com.lxzl.erp.common.domain.order.OrderQueryParam;
+import com.lxzl.erp.common.domain.order.pojo.Order;
 import com.lxzl.erp.core.annotation.ControllerLog;
 import com.lxzl.erp.core.component.ResultGenerator;
 import com.lxzl.se.common.domain.Result;
@@ -20,7 +23,13 @@ public class K3Controller extends BaseController {
 
     @RequestMapping(value = "queryOrder", method = RequestMethod.POST)
     public Result queryOrder(@RequestBody OrderQueryParam param, BindingResult validResult) {
-        ServiceResult<String, String> serviceResult = new ServiceResult<>();
+        ServiceResult<String, Page<Order>> serviceResult = new ServiceResult<>(ErrorCode.SUCCESS, new Page<Order>());
+        return resultGenerator.generate(serviceResult.getErrorCode(), serviceResult.getResult());
+    }
+
+    @RequestMapping(value = "queryOrderByNo", method = RequestMethod.POST)
+    public Result queryOrderByNo(@RequestBody OrderQueryParam param, BindingResult validResult) {
+        ServiceResult<String, Order> serviceResult = new ServiceResult<>(ErrorCode.SUCCESS, new Order());
         return resultGenerator.generate(serviceResult.getErrorCode(), serviceResult.getResult());
     }
 
