1a6b4102d24f810ef2832cfa082a6e69852dc845
liuke <liuke8080@163.com>
2018-02-26 16:20:32 +0800

    换货单 修改

--- erp-core/src/main/java/com/lxzl/erp/core/service/changeOrder/impl/ChangeOrderServiceImpl.java
+++ erp-core/src/main/java/com/lxzl/erp/core/service/changeOrder/impl/ChangeOrderServiceImpl.java
@@ -170,7 +170,7 @@ public class ChangeOrderServiceImpl implements ChangeOrderService {
                     Integer count = destSkuCountMap.get(destKey);
                     destSkuCountMap.put(destKey,count+changeOrderProduct.getChangeProductSkuCount());
                 }
-                totalChangeProductCount += changeOrderProduct.getChangeProductSkuCount();
+                totalChangeProductCount += changeOrderProduct.getChangeProductSkuCount()-changeOrderProduct.getChangeProductSkuCount();
                 ChangeOrderProductDO changeOrderProductDO = new ChangeOrderProductDO();
                 changeOrderProductDO.setSrcChangeProductSkuId(changeOrderProduct.getSrcChangeProductSkuId());
                 changeOrderProductDO.setDestChangeProductSkuId(changeOrderProduct.getDestChangeProductSkuId());
@@ -275,7 +275,7 @@ public class ChangeOrderServiceImpl implements ChangeOrderService {
                     Integer count = destMaterialCountMap.get(destKey);
                     destMaterialCountMap.put(destKey,count+changeOrderMaterial.getChangeMaterialCount());
                 }
-                totalChangeMaterialCount += changeOrderMaterial.getChangeMaterialCount();
+                totalChangeMaterialCount += changeOrderMaterial.getChangeMaterialCount()-changeOrderMaterial.getChangeMaterialCount();
                 ChangeOrderMaterialDO changeOrderMaterialDO = new ChangeOrderMaterialDO();
                 changeOrderMaterialDO.setSrcChangeMaterialId(srcMaterial.getMaterialId());
                 changeOrderMaterialDO.setDestChangeMaterialId(destMaterial.getMaterialId());
