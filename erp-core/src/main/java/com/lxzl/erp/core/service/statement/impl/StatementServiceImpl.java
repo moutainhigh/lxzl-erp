6cc34590039dc3841973f53d7963fc71f6bf8b6c
liuke <liuke8080@163.com>
2018-03-03 18:04:34 +0800

    K3换货结算

--- erp-core/src/main/java/com/lxzl/erp/core/service/statement/impl/StatementServiceImpl.java
+++ erp-core/src/main/java/com/lxzl/erp/core/service/statement/impl/StatementServiceImpl.java
@@ -18,6 +18,7 @@ import com.lxzl.erp.common.domain.user.pojo.User;
 import com.lxzl.erp.common.util.*;
 import com.lxzl.erp.core.service.amount.support.AmountSupport;
 import com.lxzl.erp.core.service.basic.impl.support.GenerateNoSupport;
+import com.lxzl.erp.core.service.k3.K3Service;
 import com.lxzl.erp.core.service.order.impl.support.OrderTimeAxisSupport;
 import com.lxzl.erp.core.service.payment.PaymentService;
 import com.lxzl.erp.core.service.permission.PermissionSupport;
@@ -30,14 +31,11 @@ import com.lxzl.erp.dataaccess.dao.mysql.changeOrder.ChangeOrderMaterialBulkMapp
 import com.lxzl.erp.dataaccess.dao.mysql.changeOrder.ChangeOrderProductEquipmentMapper;
 import com.lxzl.erp.dataaccess.dao.mysql.customer.CustomerMapper;
 import com.lxzl.erp.dataaccess.dao.mysql.k3.K3ChangeOrderMapper;
-import com.lxzl.erp.dataaccess.dao.mysql.k3.K3MappingCustomerMapper;
 import com.lxzl.erp.dataaccess.dao.mysql.k3.K3ReturnOrderDetailMapper;
 import com.lxzl.erp.dataaccess.dao.mysql.k3.K3ReturnOrderMapper;
 import com.lxzl.erp.dataaccess.dao.mysql.order.*;
-import com.lxzl.erp.dataaccess.dao.mysql.product.ProductSkuMapper;
 import com.lxzl.erp.dataaccess.dao.mysql.returnOrder.ReturnOrderMapper;
 import com.lxzl.erp.dataaccess.dao.mysql.returnOrder.ReturnOrderMaterialBulkMapper;
-import com.lxzl.erp.dataaccess.dao.mysql.returnOrder.ReturnOrderMaterialMapper;
 import com.lxzl.erp.dataaccess.dao.mysql.returnOrder.ReturnOrderProductEquipmentMapper;
 import com.lxzl.erp.dataaccess.dao.mysql.statement.StatementOrderDetailMapper;
 import com.lxzl.erp.dataaccess.dao.mysql.statement.StatementOrderMapper;
@@ -47,7 +45,8 @@ import com.lxzl.erp.dataaccess.dao.mysql.statementOrderCorrect.StatementOrderCor
 import com.lxzl.erp.dataaccess.dao.mysql.system.DataDictionaryMapper;
 import com.lxzl.erp.dataaccess.domain.changeOrder.*;
 import com.lxzl.erp.dataaccess.domain.customer.CustomerDO;
-import com.lxzl.erp.dataaccess.domain.k3.K3MappingCustomerDO;
+import com.lxzl.erp.dataaccess.domain.k3.K3ChangeOrderDO;
+import com.lxzl.erp.dataaccess.domain.k3.K3ChangeOrderDetailDO;
 import com.lxzl.erp.dataaccess.domain.k3.returnOrder.K3ReturnOrderDO;
 import com.lxzl.erp.dataaccess.domain.k3.returnOrder.K3ReturnOrderDetailDO;
 import com.lxzl.erp.dataaccess.domain.order.*;
@@ -1722,7 +1721,126 @@ public class StatementServiceImpl implements StatementService {
     @Transactional(readOnly = false, isolation = Isolation.SERIALIZABLE, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
     public ServiceResult<String, BigDecimal> createK3ChangeOrderStatement(String changeOrderNo) {
         ServiceResult<String, BigDecimal> result = new ServiceResult<>();
+        Date currentTime = new Date();
+        User loginUser = userSupport.getCurrentUser();
+        K3ChangeOrderDO k3ChangeOrderDO = k3ChangeOrderMapper.findByNo(changeOrderNo);
+        if (k3ChangeOrderDO == null) {
+            result.setErrorCode(ErrorCode.CHANGE_ORDER_NOT_EXISTS);
+            return result;
+        }
 
+        //是否计算了其他费用
+        boolean isCountOther = false;
+
+        List<StatementOrderDetailDO> addStatementOrderDetailDOList = new ArrayList<>();
+        BigDecimal otherAmount = BigDecimalUtil.add(k3ChangeOrderDO.getLogisticsAmount(), k3ChangeOrderDO.getServiceAmount());
+        List<K3ChangeOrderDetailDO> k3ChangeOrderDetailDOList = k3ChangeOrderDO.getK3ChangeOrderDetailDOList();
+        if (CollectionUtil.isNotEmpty(k3ChangeOrderDetailDOList)) {
+            for (K3ChangeOrderDetailDO k3ChangeOrderDetailDO : k3ChangeOrderDetailDOList) {
+                Integer orderItemType = null;
+                if (k3ChangeOrderDetailDO.getChangeSkuId() != null) {
+                    orderItemType = OrderItemType.ORDER_ITEM_TYPE_CHANGE_PRODUCT;
+                } else {
+                    orderItemType = OrderItemType.ORDER_ITEM_TYPE_CHANGE_MATERIAL;
+                }
+                BigDecimal productDiffAmount = k3ChangeOrderDetailDO.getProductDiffAmount();
+                BigDecimal totalProductDiffAmount = BigDecimalUtil.mul(productDiffAmount, new BigDecimal(k3ChangeOrderDetailDO.getProductCount()));
+                if (BigDecimalUtil.compare(totalProductDiffAmount, BigDecimal.ZERO) <= 0) {
+                    continue;
+                }
+                //当月差价
+                BigDecimal thisMonthDiff = BigDecimal.ZERO;
+                ServiceResult<String, com.lxzl.erp.common.domain.k3.pojo.order.Order> orderServiceResult = k3Service.queryOrder(k3ChangeOrderDetailDO.getOrderNo());
+                if (ErrorCode.SUCCESS.equals(orderServiceResult.getErrorCode())) {
+                    //如果订单是本系统订单才处理结算单
+                    OrderDO orderDO = orderMapper.findByOrderNo(k3ChangeOrderDetailDO.getOrderNo());
+                    if (orderDO == null) {
+                        continue;
+                    }
+                    //获取结算单
+                    List<StatementOrderDetailDO> statementOrderDetailDOList = statementOrderDetailMapper.findByOrderId(orderDO.getId());
+                    //获取下次结算点（如果没有下次了，就当天）
+
+                    StatementOrderDetailDO nextStatement = null;
+                    for (int i = 0; i < statementOrderDetailDOList.size(); i++) {
+                        if (nextStatement != null) {
+                            break;
+                        }
+                        StatementOrderDetailDO statementOrderDetailDO = statementOrderDetailDOList.get(i);
+                        //换货时间在结算开始时间和结算结束时间之间，且结算类型为租金，就计算换货时间到结算结束时间期间
+                        if (statementOrderDetailDO.getStatementStartTime().getTime() < k3ChangeOrderDO.getChangeTime().getTime() &&
+                                statementOrderDetailDO.getStatementEndTime().getTime() >= k3ChangeOrderDO.getChangeTime().getTime()) {
+                            //按天算钱
+                            if (OrderRentType.RENT_TYPE_DAY.equals(k3ChangeOrderDetailDO.getRentType())) {
+                                int dayCount = DateUtil.daysBetween(statementOrderDetailDO.getStatementStartTime(), statementOrderDetailDO.getStatementEndTime());
+                                thisMonthDiff = BigDecimalUtil.mul(new BigDecimal(dayCount * k3ChangeOrderDetailDO.getProductCount()), k3ChangeOrderDetailDO.getProductDiffAmount());
+                            } else if (OrderRentType.RENT_TYPE_MONTH.equals(k3ChangeOrderDetailDO.getRentType())) {
+                                //按月算钱
+                                thisMonthDiff = amountSupport.calculateRentAmount(k3ChangeOrderDO.getChangeTime(), statementOrderDetailDO.getStatementEndTime(), totalProductDiffAmount);
+                            }
+                            if (i != statementOrderDetailDOList.size() - 1) {
+                                nextStatement = statementOrderDetailDOList.get(i + 1);
+                            } else {
+                                StatementOrderDetailDO newStatementOrderDetailDO = new StatementOrderDetailDO();
+                                newStatementOrderDetailDO.setCustomerId(statementOrderDetailDO.getCustomerId());
+                                newStatementOrderDetailDO.setOrderId(statementOrderDetailDO.getOrderId());
+                                newStatementOrderDetailDO.setOrderItemType(statementOrderDetailDO.getOrderItemType());
+                                newStatementOrderDetailDO.setOrderItemReferId(statementOrderDetailDO.getOrderItemReferId());
+                                newStatementOrderDetailDO.setStatementStartTime(k3ChangeOrderDO.getChangeTime());
+                                newStatementOrderDetailDO.setStatementExpectPayTime(k3ChangeOrderDO.getChangeTime());
+                                newStatementOrderDetailDO.setStatementEndTime(k3ChangeOrderDO.getChangeTime());
+                                nextStatement = newStatementOrderDetailDO;
+                            }
+                        }
+                    }
+                    if (nextStatement != null) {
+                        //如果差价大于0
+                        if (BigDecimalUtil.compare(thisMonthDiff, BigDecimal.ZERO) > 0) {
+
+                            StatementOrderDetailDO changeStatementOrderDetailDO = buildStatementOrderDetailDO(nextStatement.getCustomerId(), OrderType.ORDER_TYPE_CHANGE,
+                                    nextStatement.getOrderId(), orderItemType, Integer.parseInt(k3ChangeOrderDetailDO.getOrderItemId()),
+                                    nextStatement.getStatementExpectPayTime(), nextStatement.getStatementStartTime(), nextStatement.getStatementEndTime(),
+                                    thisMonthDiff, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, currentTime, loginUser.getUserId());
+                            addStatementOrderDetailDOList.add(changeStatementOrderDetailDO);
+                        }
+                        //如果其他费用没有计算过，且其他费用大于0
+                        if (!isCountOther && BigDecimalUtil.compare(otherAmount, BigDecimal.ZERO) > 0) {
+                            StatementOrderDetailDO changeStatementOrderDetailDO = buildStatementOrderDetailDO(nextStatement.getCustomerId(), OrderType.ORDER_TYPE_CHANGE,
+                                    nextStatement.getOrderId(), OrderItemType.ORDER_ITEM_TYPE_CHANGE_OTHER, 0,
+                                    nextStatement.getStatementExpectPayTime(), nextStatement.getStatementStartTime(), nextStatement.getStatementEndTime(),
+                                    BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, otherAmount, currentTime, loginUser.getUserId());
+                            addStatementOrderDetailDOList.add(changeStatementOrderDetailDO);
+                            isCountOther = true;
+                        }
+                    }
+
+
+                    for (int i = 0; i < statementOrderDetailDOList.size(); i++) {
+                        StatementOrderDetailDO statementOrderDetailDO = statementOrderDetailDOList.get(i);
+                        //结算开始时间小于等于换货时间，不处理，不是租金不处理
+                        if (!StatementDetailType.STATEMENT_DETAIL_TYPE_RENT.equals(statementOrderDetailDO.getStatementDetailType()) &&
+                                statementOrderDetailDO.getStatementStartTime().getTime() <= k3ChangeOrderDO.getChangeTime().getTime()) {
+                            continue;
+                        }
+                        BigDecimal diff = BigDecimal.ZERO;
+                        //按天算钱
+                        if (OrderRentType.RENT_TYPE_DAY.equals(k3ChangeOrderDetailDO.getRentType())) {
+                            int dayCount = DateUtil.daysBetween(statementOrderDetailDO.getStatementStartTime(), statementOrderDetailDO.getStatementEndTime());
+                            diff = BigDecimalUtil.mul(new BigDecimal(dayCount * k3ChangeOrderDetailDO.getProductCount()), k3ChangeOrderDetailDO.getProductDiffAmount());
+                        } else if (OrderRentType.RENT_TYPE_MONTH.equals(k3ChangeOrderDetailDO.getRentType())) {
+                            //按月算钱
+                            diff = amountSupport.calculateRentAmount(statementOrderDetailDO.getStatementStartTime(), statementOrderDetailDO.getStatementEndTime(), totalProductDiffAmount);
+                        }
+                        StatementOrderDetailDO changeStatementOrderDetailDO = buildStatementOrderDetailDO(statementOrderDetailDO.getCustomerId(), OrderType.ORDER_TYPE_CHANGE,
+                                statementOrderDetailDO.getOrderId(), orderItemType, Integer.parseInt(k3ChangeOrderDetailDO.getOrderItemId()),
+                                statementOrderDetailDO.getStatementExpectPayTime(), statementOrderDetailDO.getStatementStartTime(), statementOrderDetailDO.getStatementEndTime(),
+                                diff, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, currentTime, loginUser.getUserId());
+                        addStatementOrderDetailDOList.add(changeStatementOrderDetailDO);
+                    }
+                }
+            }
+        }
+        saveStatementOrder(addStatementOrderDetailDOList, currentTime, loginUser.getUserId());
         result.setErrorCode(ErrorCode.SUCCESS);
         return result;
     }
@@ -2545,14 +2663,8 @@ public class StatementServiceImpl implements StatementService {
     private K3ChangeOrderMapper k3ChangeOrderMapper;
 
     @Autowired
-    private K3MappingCustomerMapper k3MappingCustomerMapper;
-
-    @Autowired
     private K3ReturnOrderDetailMapper k3ReturnOrderDetailMapper;
 
     @Autowired
-    private ProductSkuMapper productSkuMapper;
-
-    @Autowired
-    private ReturnOrderMaterialMapper returnOrderMaterialMapper;
+    private K3Service k3Service;
 }
