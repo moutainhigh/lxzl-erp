5861c2e3aa57e88e67529aa8d01e7aaccdbf3a9f
kai <porysung55@msn.com>
2018-03-30 19:54:57 +0800

    edit

--- erp-web/src/main/java/com/lxzl/erp/web/controller/WorkflowController.java
+++ erp-web/src/main/java/com/lxzl/erp/web/controller/WorkflowController.java
@@ -97,6 +97,12 @@ public class WorkflowController extends BaseController {
         return resultGenerator.generate(serviceResult.getErrorCode(), serviceResult.getResult());
     }
 
+    @RequestMapping(value = "workflowImportData", method = RequestMethod.GET)
+    public Result workflowImportData(@RequestBody WorkflowTemplateQueryParam workflowTemplateQueryParam, BindingResult bindingResult) {
+        ServiceResult<String, String> serviceResult = workflowService.workflowImportData();
+        return resultGenerator.generate(serviceResult.getErrorCode(), serviceResult.getResult());
+    }
+
 
     @Autowired
     private WorkflowService workflowService;
