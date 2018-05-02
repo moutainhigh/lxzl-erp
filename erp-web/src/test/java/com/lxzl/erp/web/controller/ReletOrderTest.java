d226b804d59893884dc050f6e49f24977cf4b1e4
zhaozixuan <245087147@qq.com>
2018-05-02 14:40:33 +0800

    补充

--- erp-web/src/test/java/com/lxzl/erp/web/controller/ReletOrderTest.java
+++ erp-web/src/test/java/com/lxzl/erp/web/controller/ReletOrderTest.java
@@ -134,7 +134,7 @@ public class ReletOrderTest extends ERPUnTransactionalTest {
     @Test
     public void testQueryAllReletOrder() throws Exception{
         ReletOrderQueryParam reletOrderQueryParam = new ReletOrderQueryParam();
-//        reletOrderQueryParam.setBuyerCustomerId(704447);
+        reletOrderQueryParam.setBuyerCustomerId(704447);
 //        reletOrderQueryParam.setBuyerCustomerNo("LXO-20180305-0755-00028");
 //        reletOrderQueryParam.setBuyerRealName("湖北华天翼建设工程有限公司");
 ////        reletOrderQueryParam.setCreateEndTime();
