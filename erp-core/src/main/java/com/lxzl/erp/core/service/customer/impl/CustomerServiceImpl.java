0631a886bfa406dccc782afaf126c7c030eb0f5d
liuke <liuke8080@163.com>
2018-05-09 15:32:49 +0800

    公司客户 个人客户创建时  同时生成账户

--- erp-core/src/main/java/com/lxzl/erp/core/service/customer/impl/CustomerServiceImpl.java
+++ erp-core/src/main/java/com/lxzl/erp/core/service/customer/impl/CustomerServiceImpl.java
@@ -246,7 +246,7 @@ public class CustomerServiceImpl implements CustomerService {
 
         // 添加客户变更记录
         createCustomerUpdateLog(customerDO.getId(), customerDO.getOwner(), customerDO.getUnionUser(), now, 0, 0, null, null);
-
+        paymentService.queryCustomerAccount(customerDO.getCustomerNo());
         webServiceHelper.post(PostK3OperatorType.POST_K3_OPERATOR_TYPE_NULL, PostK3Type.POST_K3_TYPE_CUSTOMER, ConverterUtil.convert(customerDO, Customer.class), true);
         serviceResult.setErrorCode(ErrorCode.SUCCESS);
         serviceResult.setResult(customerDO.getCustomerNo());
@@ -327,7 +327,7 @@ public class CustomerServiceImpl implements CustomerService {
 
         // 添加客户变更记录
         createCustomerUpdateLog(customerDO.getId(), customerDO.getOwner(), customerDO.getUnionUser(), now, 0, 0, null, null);
-
+        paymentService.queryCustomerAccount(customerDO.getCustomerNo());
         webServiceHelper.post(PostK3OperatorType.POST_K3_OPERATOR_TYPE_NULL, PostK3Type.POST_K3_TYPE_CUSTOMER, ConverterUtil.convert(customerDO, Customer.class), true);
         serviceResult.setErrorCode(ErrorCode.SUCCESS);
         serviceResult.setResult(customerDO.getCustomerNo());
