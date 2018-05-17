ebd5148588445e01377a9670f1426bb27f2a146f
xiaoluyu <xly19930523@163.com>
2018-05-17 20:35:20 +0800

    修改属地化错误
    Signed-off-by: xiaoluyu <xly19930523@163.com>

--- erp-core/src/main/java/com/lxzl/erp/core/service/bank/impl/importSlip/ImportChinaBank.java
+++ erp-core/src/main/java/com/lxzl/erp/core/service/bank/impl/importSlip/ImportChinaBank.java
@@ -118,13 +118,9 @@ public class ImportChinaBank {
                 return serviceResult;
             }
             //保存  银行对公流水明细表
-            Integer isLocalization = null;
-            if (CommonConstant.HEADER_COMPANY_ID.equals(bankSlipDO.getSubCompanyId())) {
-                isLocalization = CommonConstant.COMMON_CONSTANT_NO;
-            }
             for (BankSlipDetailDO bankSlipDetailDO : bankSlipDetailDOList) {
                 bankSlipDetailDO.setBankSlipId(bankSlipDO.getId());
-                bankSlipDetailDO.setIsLocalization(isLocalization);
+                bankSlipDetailDO.setIsLocalization(CommonConstant.COMMON_CONSTANT_NO);
                 bankSlipDetailDO.setSubCompanyId(bankSlipDO.getSubCompanyId());
             }
             //改为批量保存
