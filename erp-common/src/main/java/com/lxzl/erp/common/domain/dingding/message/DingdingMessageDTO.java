726498723c81619b0d68050184eabcdd5695f0f4
daiqi <466608943@qq.com>
2018-05-21 14:16:22 +0800

    添加信息表

---
+++ erp-common/src/main/java/com/lxzl/erp/common/domain/dingding/message/DingdingMessageDTO.java
@@ -0,0 +1,10 @@
+package com.lxzl.erp.common.domain.dingding.message;
+
+import com.lxzl.erp.common.domain.dingding.DingdingBaseDTO;
+
+/**
+ * @author daiqi
+ * @create 2018-05-21 13:44
+ */
+public class DingdingMessageDTO extends DingdingBaseDTO{
+}
