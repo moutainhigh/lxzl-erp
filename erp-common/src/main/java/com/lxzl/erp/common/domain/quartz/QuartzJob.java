c28165c45e1623453a2e51220504618ac76933e8
huahongbin <hhbvictory@163.com>
2018-05-24 10:57:21 +0800

    添加定时任务参数验证

--- erp-common/src/main/java/com/lxzl/erp/common/domain/quartz/QuartzJob.java
+++ erp-common/src/main/java/com/lxzl/erp/common/domain/quartz/QuartzJob.java
@@ -1,5 +1,12 @@
 package com.lxzl.erp.common.domain.quartz;
 
+import com.lxzl.erp.common.constant.ErrorCode;
+import com.lxzl.erp.common.domain.validGroup.quartz.AddOrUpdateJobGroup;
+import com.lxzl.erp.common.domain.validGroup.quartz.JobGroup;
+import com.lxzl.erp.common.domain.validGroup.quartz.SchedNameGroup;
+import com.lxzl.erp.common.domain.validGroup.quartz.TriggerGroup;
+
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Map;
@@ -14,21 +21,25 @@ public class QuartzJob implements Serializable {
     /**
      * sched名称
      */
+    @NotNull(message = ErrorCode.QUARTZ_SCHED_NAME_NOT_NULL, groups = {AddOrUpdateJobGroup.class, SchedNameGroup.class})
     private String schedName;
 
     /**
      * 任务名称
      */
+    @NotNull(message = ErrorCode.QUARTZ_JOB_NAME_NOT_NULL, groups = {AddOrUpdateJobGroup.class, JobGroup.class})
     private String jobName;
 
     /**
      * 任务组
      */
+    @NotNull(message = ErrorCode.QUARTZ_JOB_GROUP_NOT_NULL, groups = {AddOrUpdateJobGroup.class, JobGroup.class})
     private String jobGroup;
 
     /**
      * 任务的具体类名, 可以是全路径的java类, 也可以是被spring管理的简单类名
      */
+    @NotNull(message = ErrorCode.QUARTZ_JOB_CLASS_NAME_NOT_NULL, groups = {AddOrUpdateJobGroup.class})
     private String jobClassName;
 
     /**
@@ -39,26 +50,31 @@ public class QuartzJob implements Serializable {
     /**
      * 任务中断（如断电）是否是重新执行
      */
+    @NotNull(message = ErrorCode.QUARTZ_RECOVERY_FLAG_NOT_NULL, groups = {AddOrUpdateJobGroup.class})
     private boolean recoveryFlag;
 
     /**
      * 触发器的名称（如：每天2点触发库存对账）
      */
+    @NotNull(message = ErrorCode.QUARTZ_TRIGGER_NAME_NOT_NULL, groups = {AddOrUpdateJobGroup.class, TriggerGroup.class})
     private String triggerName;
 
     /**
      * 触发器分组
      */
+    @NotNull(message = ErrorCode.QUARTZ_TRIGGER_GROUP_NOT_NULL, groups = {AddOrUpdateJobGroup.class, TriggerGroup.class})
     private String triggerGroup;
 
     /**
      * 是否是cron的表达式
      */
+    @NotNull(message = ErrorCode.QUARTZ_CRON_TRIGGER_FLAG_NOT_NULL, groups = {AddOrUpdateJobGroup.class})
     private boolean cronTriggerFlag;
 
     /**
      * 任务触发时间的表达式(isCronTrigger等于true时为cron的表达式，isCronTrigger等于false时为间隔时间，单位秒)
      */
+    @NotNull(message = ErrorCode.QUARTZ_EXPRESSION_NOT_NULL, groups = {AddOrUpdateJobGroup.class})
     private String expression;
 
     /**
