4ea2642fa645bb00cb99f96dd3bc60fb228d488d
刘可 <75002425@qq.com>
2018-05-31 02:43:18 +0000

    Merge branch '_liuke_returnStatement' into 'develop_v1.0.1'
    退货回调兼容k3老订单
    
    See merge request !747

--- erp-core/src/main/java/com/lxzl/erp/core/service/product/impl/support/ProductSupport.java
+++ erp-core/src/main/java/com/lxzl/erp/core/service/product/impl/support/ProductSupport.java
@@ -2,12 +2,22 @@ package com.lxzl.erp.core.service.product.impl.support;
 
 import com.lxzl.erp.common.constant.ErrorCode;
 import com.lxzl.erp.common.domain.ServiceResult;
+import com.lxzl.erp.common.domain.order.pojo.OrderMaterial;
+import com.lxzl.erp.common.util.CollectionUtil;
+import com.lxzl.erp.dataaccess.dao.mysql.order.OrderMapper;
+import com.lxzl.erp.dataaccess.dao.mysql.order.OrderMaterialMapper;
+import com.lxzl.erp.dataaccess.dao.mysql.order.OrderProductMapper;
 import com.lxzl.erp.dataaccess.dao.mysql.product.ProductSkuMapper;
+import com.lxzl.erp.dataaccess.domain.order.OrderDO;
+import com.lxzl.erp.dataaccess.domain.order.OrderMaterialDO;
+import com.lxzl.erp.dataaccess.domain.order.OrderProductDO;
 import com.lxzl.erp.dataaccess.domain.product.ProductSkuDO;
+import com.lxzl.se.common.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * 描述: ${DESCRIPTION}
@@ -20,6 +30,12 @@ public class ProductSupport {
 
     @Autowired
     private ProductSkuMapper productSkuMapper;
+    @Autowired
+    private OrderProductMapper orderProductMapper;
+    @Autowired
+    private OrderMaterialMapper orderMaterialMapper;
+    @Autowired
+    private OrderMapper orderMapper;
 
     public String operateSkuStock(Integer skuId, Integer opStock) {
         Date currentTime = new Date();
@@ -41,5 +57,61 @@ public class ProductSupport {
         }else{
             return false;
         }
+    }
+    public boolean isProduct(String productNo) {
+       return !isMaterial(productNo);
+    }
+
+    /**
+     * 兼容erp订单和k3订单商品项
+     * @param orderNo
+     * @param orderItemId
+     * @param orderEntry
+     * @return
+     */
+    public OrderProductDO getOrderProductDO(String orderNo , String orderItemId , String orderEntry){
+        OrderProductDO orderProductDO = null;
+        if(StringUtil.isNotEmpty(orderItemId)&&!"0".equals(orderItemId)){
+            orderProductDO = orderProductMapper.findById(Integer.parseInt(orderItemId));
+        }else if(StringUtil.isNotEmpty(orderEntry)){
+            Integer oe = Integer.parseInt(orderEntry);
+            OrderDO orderDO = orderMapper.findByOrderNo(orderNo);
+            if(orderDO!=null&& CollectionUtil.isNotEmpty(orderDO.getOrderProductDOList())){
+                List<OrderProductDO> orderProductDOList = orderDO.getOrderProductDOList();
+                for(OrderProductDO op : orderProductDOList){
+                    if(oe.equals(op.getFEntryID())){
+                        orderProductDO = op;
+                        break;
+                    }
+                }
+            }
+        }
+        return orderProductDO;
+    }
+    /**
+     * 兼容erp订单和k3订单配件项
+     * @param orderNo
+     * @param orderItemId
+     * @param orderEntry
+     * @return
+     */
+    public OrderMaterialDO getOrderMaterialDO(String orderNo , String orderItemId , String orderEntry){
+        OrderMaterialDO orderMaterialDO = null;
+        if(StringUtil.isNotEmpty(orderItemId)&&!"0".equals(orderItemId)){
+            orderMaterialDO = orderMaterialMapper.findById(Integer.parseInt(orderItemId));
+        }else if(StringUtil.isNotEmpty(orderEntry)){
+            Integer oe = Integer.parseInt(orderEntry);
+            OrderDO orderDO = orderMapper.findByOrderNo(orderNo);
+            if(orderDO!=null&& CollectionUtil.isNotEmpty(orderDO.getOrderMaterialDOList())){
+                List<OrderMaterialDO> orderMaterialDOList = orderDO.getOrderMaterialDOList();
+                for(OrderMaterialDO om : orderMaterialDOList){
+                    if(oe.equals(om.getFEntryID())){
+                        orderMaterialDO = om;
+                        break;
+                    }
+                }
+            }
+        }
+        return orderMaterialDO;
     }
 }
