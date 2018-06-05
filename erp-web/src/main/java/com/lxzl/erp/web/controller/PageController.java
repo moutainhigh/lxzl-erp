af362ff638907c1579c1bdc58f363dbe33194f58
liwenbin <weblee18@163.com>
2018-06-05 13:47:50 +0800

    动态sql页面

--- erp-web/src/main/java/com/lxzl/erp/web/controller/PageController.java
+++ erp-web/src/main/java/com/lxzl/erp/web/controller/PageController.java
@@ -977,6 +977,12 @@ public class PageController extends BaseController {
         return "/privateManage/request";
     }
 
+
+    @RequestMapping("/data-analysis/dynamicSql")
+    public String systemManageDynamicSql() {
+        return "/dataAnalysisManage/dynamicSql";
+    }
+
     //上传附件
     @RequestMapping("/jurnal-attachment-list/file/upload")
     public String jurnalAttachmentList() {return "/component/jurnalAmount/upload";}
