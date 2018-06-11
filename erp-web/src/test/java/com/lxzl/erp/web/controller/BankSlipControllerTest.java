60d2b7fa40c4bb0acf49fb978b084091e4c8bafd
刘可 <75002425@qq.com>
2018-06-11 14:48:02 +0800

    Revert "Merge branch '_sunzhipeng_confirmBankSlip' into 'develop_v1.0.2'"
    This reverts merge request !834

--- erp-web/src/test/java/com/lxzl/erp/web/controller/BankSlipControllerTest.java
+++ erp-web/src/test/java/com/lxzl/erp/web/controller/BankSlipControllerTest.java
@@ -132,7 +132,7 @@ public class BankSlipControllerTest extends ERPUnTransactionalTest {
     @Test
     public void verifyBankSlipDetail() throws Exception {
         BankSlip bankSlip = new BankSlip();
-        bankSlip.setBankSlipId(4);
+        bankSlip.setBankSlipId(18);
         TestResult result = getJsonTestResult("/bankSlip/confirmBankSlip", bankSlip);
     }
 
