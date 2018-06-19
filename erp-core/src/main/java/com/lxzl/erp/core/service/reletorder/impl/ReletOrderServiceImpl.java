6cb119d2f5eb828c3c2b5cb9c3b082cc036fc1a8
zhaozixuan <245087147@qq.com>
2018-06-19 16:52:17 +0800

    补充

--- erp-core/src/main/java/com/lxzl/erp/core/service/reletorder/impl/ReletOrderServiceImpl.java
+++ erp-core/src/main/java/com/lxzl/erp/core/service/reletorder/impl/ReletOrderServiceImpl.java
@@ -720,9 +720,9 @@ public class ReletOrderServiceImpl implements ReletOrderService {
 
         BigDecimal resultFloorValue = BigDecimalUtil.mul(originUnitAmount, floorValue);
         BigDecimal resultCeilValue = BigDecimalUtil.mul(originUnitAmount, ceilValue);
-        BigDecimal zeroValue = new BigDecimal(0);
+
         //原价和现价都为零时  无需审核
-        if (BigDecimalUtil.compare(unitAmount, zeroValue) == 0 && BigDecimalUtil.compare(originUnitAmount, zeroValue) == 0){
+        if (BigDecimalUtil.compare(unitAmount, BigDecimal.ZERO) == 0 && BigDecimalUtil.compare(originUnitAmount, BigDecimal.ZERO) == 0){
             return false;
         }
 
