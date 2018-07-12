d959df1f6132f7cd57ddbfd4a5f65a37f81da1ba
huahongbin <hhbvictory@163.com>
2018-07-12 19:01:12 +0800

    添加批量创建k3退货结算单接口

---
+++ erp-web/src/main/java/com/lxzl/erp/web/controller/BatchController.java
@@ -0,0 +1,28 @@
+package com.lxzl.erp.web.controller;
+
+import com.lxzl.erp.common.domain.statement.BatchCreateK3ReturnOrderStatementParam;
+import com.lxzl.erp.core.annotation.ControllerLog;
+import com.lxzl.erp.core.component.ResultGenerator;
+import com.lxzl.erp.core.service.batch.BatchService;
+import com.lxzl.se.common.domain.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RequestMapping("/batch")
+@Controller
+@ControllerLog
+public class BatchController {
+
+    @Autowired
+    private BatchService batchService;
+    @Autowired
+    private ResultGenerator resultGenerator;
+
+    @RequestMapping(value = "batchCreateK3ReturnOrderStatement", method = RequestMethod.POST)
+    public Result batchCreateK3ReturnOrderStatement(@RequestBody BatchCreateK3ReturnOrderStatementParam batchCreateK3ReturnOrderStatementParam) {
+        return resultGenerator.generate(batchService.batchCreateK3ReturnOrderStatement(batchCreateK3ReturnOrderStatementParam.getReturnOrderNoList()));
+    }
+}
