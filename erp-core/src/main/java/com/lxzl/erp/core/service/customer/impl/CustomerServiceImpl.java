52a0a905d513e1f4bb9509bd0055f5a447bd5624
Peng Bin Jie <603742275@qq.com>
2018-07-26 14:06:36 +0800

    更改原有数据如果所需设备的租赁时长为null时，总租金直接为0

--- erp-core/src/main/java/com/lxzl/erp/core/service/customer/impl/CustomerServiceImpl.java
+++ erp-core/src/main/java/com/lxzl/erp/core/service/customer/impl/CustomerServiceImpl.java
@@ -1181,7 +1181,7 @@ public class CustomerServiceImpl implements CustomerService {
                     BigDecimal totalRentPrice = BigDecimalUtil.mul(countPrice,new BigDecimal(customerCompanyNeed.getRentTimeLength()));
                     customerCompanyNeed.setTotalProductRentPrice(totalRentPrice);
                 }else{
-                    customerCompanyNeed.setTotalProductRentPrice(countPrice);
+                    customerCompanyNeed.setTotalProductRentPrice(BigDecimal.ZERO);
                 }
             }
             //计算总列表的金额
