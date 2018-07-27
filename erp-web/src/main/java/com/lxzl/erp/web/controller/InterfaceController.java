50b46c720a43a3bde1c8759c479161bb5ba70fd1
liuge <948079594@qq.com>
2018-07-27 21:39:16 +0800

    活动用户注册 part1 开放用户注册的接口

--- erp-web/src/main/java/com/lxzl/erp/web/controller/InterfaceController.java
+++ erp-web/src/main/java/com/lxzl/erp/web/controller/InterfaceController.java
@@ -1,9 +1,9 @@
 package com.lxzl.erp.web.controller;
 
+import com.lxzl.erp.common.constant.CommonConstant;
 import com.lxzl.erp.common.constant.ErrorCode;
 import com.lxzl.erp.common.domain.Page;
 import com.lxzl.erp.common.domain.ServiceResult;
-import com.lxzl.erp.common.domain.company.SubCompanyQueryParam;
 import com.lxzl.erp.common.domain.company.pojo.SubCompany;
 import com.lxzl.erp.common.domain.customer.pojo.Customer;
 import com.lxzl.erp.common.domain.erpInterface.customer.InterfaceCustomerAccountLogParam;
@@ -15,8 +15,9 @@ import com.lxzl.erp.common.domain.erpInterface.subCompany.InterfaceSubCompanyQue
 import com.lxzl.erp.common.domain.erpInterface.weiXin.InterfaceWeixinChargeParam;
 import com.lxzl.erp.common.domain.order.pojo.Order;
 import com.lxzl.erp.common.domain.payment.account.pojo.ChargeRecord;
-import com.lxzl.erp.common.domain.payment.account.pojo.CustomerAccountLogSummary;
 import com.lxzl.erp.common.domain.statement.pojo.StatementOrder;
+import com.lxzl.erp.common.domain.user.pojo.User;
+import com.lxzl.erp.common.domain.validGroup.customer.AddCustomerPersonGroup;
 import com.lxzl.erp.common.domain.validGroup.customer.QueryCustomerNameGroup;
 import com.lxzl.erp.common.domain.validGroup.customer.QueryCustomerNoGroup;
 import com.lxzl.erp.core.annotation.ControllerLog;
@@ -27,6 +28,8 @@ import com.lxzl.erp.core.service.customer.CustomerService;
 import com.lxzl.erp.core.service.order.OrderService;
 import com.lxzl.erp.core.service.payment.PaymentService;
 import com.lxzl.erp.core.service.statement.StatementService;
+import com.lxzl.erp.core.service.user.UserService;
+import com.lxzl.erp.core.service.user.impl.support.UserSupport;
 import com.lxzl.erp.web.util.NetworkUtil;
 import com.lxzl.se.common.domain.Result;
 import com.lxzl.se.web.controller.BaseController;
@@ -39,6 +42,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 /**
@@ -76,11 +80,20 @@ public class InterfaceController extends BaseController {
     @Autowired
     private CompanyService companyService;
 
+    @Autowired
+    private UserSupport userSupport;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HttpSession httpSession;
+
     @RequestMapping(value = "queryOrderByNo", method = RequestMethod.POST)
     public Result queryOrderByNo(@RequestBody @Validated(QueryCustomerNoGroup.class) InterfaceOrderQueryParam interfaceOrderQueryParam, BindingResult validResult) {
 
         boolean erpIdentity = businessSystemConfigService.verifyErpIdentity(interfaceOrderQueryParam.getErpAppId(), interfaceOrderQueryParam.getErpAppSecret());
-        if(erpIdentity){
+        if (erpIdentity) {
             ServiceResult<String, Order> serviceResult = orderService.queryOrderByNo(interfaceOrderQueryParam.getOrderNo());
             return resultGenerator.generate(serviceResult.getErrorCode(), serviceResult.getResult());
         }
@@ -93,7 +106,7 @@ public class InterfaceController extends BaseController {
     public Result queryAllOrder(@RequestBody @Validated InterfaceOrderQueryParam interfaceOrderQueryParam, BindingResult validResult) {
 
         boolean erpIdentity = businessSystemConfigService.verifyErpIdentity(interfaceOrderQueryParam.getErpAppId(), interfaceOrderQueryParam.getErpAppSecret());
-        if(erpIdentity){
+        if (erpIdentity) {
             ServiceResult<String, Page<Order>> serviceResult = orderService.queryOrderByUserIdInterface(interfaceOrderQueryParam);
             return resultGenerator.generate(serviceResult.getErrorCode(), serviceResult.getResult());
         }
@@ -106,7 +119,7 @@ public class InterfaceController extends BaseController {
     public Result queryCustomerByName(@RequestBody @Validated(QueryCustomerNameGroup.class) InterfaceCustomerQueryParam interfaceCustomerQueryParam, BindingResult validResult) {
 
         boolean erpIdentity = businessSystemConfigService.verifyErpIdentity(interfaceCustomerQueryParam.getErpAppId(), interfaceCustomerQueryParam.getErpAppSecret());
-        if(erpIdentity){
+        if (erpIdentity) {
             ServiceResult<String, Customer> serviceResult = customerService.queryCustomerByCustomerName(interfaceCustomerQueryParam.getCustomerName());
             return resultGenerator.generate(serviceResult.getErrorCode(), serviceResult.getResult());
         }
@@ -117,7 +130,7 @@ public class InterfaceController extends BaseController {
     @RequestMapping(value = "queryStatementOrder", method = RequestMethod.POST)
     public Result queryStatementOrder(@RequestBody @Validated InterfaceStatementOrderQueryParam interfaceStatementOrderQueryParam, BindingResult validResult) {
         boolean erpIdentity = businessSystemConfigService.verifyErpIdentity(interfaceStatementOrderQueryParam.getErpAppId(), interfaceStatementOrderQueryParam.getErpAppSecret());
-        if(erpIdentity){
+        if (erpIdentity) {
             ServiceResult<String, Page<StatementOrder>> serviceResult = statementService.queryStatementOrder(interfaceStatementOrderQueryParam);
             return resultGenerator.generate(serviceResult.getErrorCode(), serviceResult.getResult());
         }
@@ -128,7 +141,7 @@ public class InterfaceController extends BaseController {
     @RequestMapping(value = "queryStatementOrderDetail", method = RequestMethod.POST)
     public Result queryStatementOrderDetail(@RequestBody @Validated InterfaceStatementOrderQueryParam interfaceStatementOrderQueryParam, BindingResult validResult) {
         boolean erpIdentity = businessSystemConfigService.verifyErpIdentity(interfaceStatementOrderQueryParam.getErpAppId(), interfaceStatementOrderQueryParam.getErpAppSecret());
-        if(erpIdentity){
+        if (erpIdentity) {
             ServiceResult<String, StatementOrder> serviceResult = statementService.queryStatementOrderDetail(interfaceStatementOrderQueryParam.getStatementOrderNo());
             return resultGenerator.generate(serviceResult.getErrorCode(), serviceResult.getResult());
         }
@@ -139,7 +152,7 @@ public class InterfaceController extends BaseController {
     @RequestMapping(value = "weixinPayStatementOrder", method = RequestMethod.POST)
     public Result weixinPayStatementOrder(@RequestBody @Validated InterfaceStatementOrderPayParam interfaceStatementOrderPayParam, BindingResult validResult) {
         boolean erpIdentity = businessSystemConfigService.verifyErpIdentity(interfaceStatementOrderPayParam.getErpAppId(), interfaceStatementOrderPayParam.getErpAppSecret());
-        if(erpIdentity){
+        if (erpIdentity) {
             ServiceResult<String, String> serviceResult = statementService.weixinPayStatementOrder(interfaceStatementOrderPayParam.getStatementOrderNo(), interfaceStatementOrderPayParam.getOpenId(), NetworkUtil.getIpAddress(request));
             return resultGenerator.generate(serviceResult.getErrorCode(), serviceResult.getResult());
         }
@@ -150,8 +163,8 @@ public class InterfaceController extends BaseController {
     @RequestMapping(value = "wechatCharge", method = RequestMethod.POST)
     public Result wechatCharge(@RequestBody @Validated InterfaceWeixinChargeParam interfaceWeixinChargeParam, BindingResult validResult) {
         boolean erpIdentity = businessSystemConfigService.verifyErpIdentity(interfaceWeixinChargeParam.getErpAppId(), interfaceWeixinChargeParam.getErpAppSecret());
-        if(erpIdentity){
-            ServiceResult<String, String> serviceResult = paymentService.wechatCharge(interfaceWeixinChargeParam.getCustomerNo(),interfaceWeixinChargeParam.getAmount(),interfaceWeixinChargeParam.getOpenId(),NetworkUtil.getIpAddress(request));
+        if (erpIdentity) {
+            ServiceResult<String, String> serviceResult = paymentService.wechatCharge(interfaceWeixinChargeParam.getCustomerNo(), interfaceWeixinChargeParam.getAmount(), interfaceWeixinChargeParam.getOpenId(), NetworkUtil.getIpAddress(request));
             return resultGenerator.generate(serviceResult.getErrorCode(), serviceResult.getResult());
         }
         return resultGenerator.generate(ErrorCode.BUSINESS_SYSTEM_ERROR);
@@ -160,7 +173,7 @@ public class InterfaceController extends BaseController {
     @RequestMapping(value = "queryChargeRecordPage", method = RequestMethod.POST)
     public Result queryChargeRecordPage(@RequestBody @Validated(QueryCustomerNoGroup.class) InterfaceCustomerQueryParam interfaceCustomerQueryParam, BindingResult validResult) {
         boolean erpIdentity = businessSystemConfigService.verifyErpIdentity(interfaceCustomerQueryParam.getErpAppId(), interfaceCustomerQueryParam.getErpAppSecret());
-        if(erpIdentity){
+        if (erpIdentity) {
             ServiceResult<String, Page<ChargeRecord>> serviceResult = paymentService.queryChargeRecordPage(interfaceCustomerQueryParam.getCustomerNo());
             return resultGenerator.generate(serviceResult.getErrorCode(), serviceResult.getResult());
         }
@@ -170,7 +183,7 @@ public class InterfaceController extends BaseController {
     @RequestMapping(value = "queryCustomerByNo", method = RequestMethod.POST)
     public Result queryCustomerByNo(@RequestBody @Validated(QueryCustomerNoGroup.class) InterfaceCustomerQueryParam interfaceCustomerQueryParam, BindingResult validResult) {
         boolean erpIdentity = businessSystemConfigService.verifyErpIdentity(interfaceCustomerQueryParam.getErpAppId(), interfaceCustomerQueryParam.getErpAppSecret());
-        if(erpIdentity){
+        if (erpIdentity) {
             ServiceResult<String, Customer> serviceResult = customerService.queryCustomerByNo(interfaceCustomerQueryParam.getCustomerNo());
             return resultGenerator.generate(serviceResult.getErrorCode(), serviceResult.getResult());
         }
@@ -180,7 +193,7 @@ public class InterfaceController extends BaseController {
     @RequestMapping(value = "weixinQueryCustomerAccountLogPage", method = RequestMethod.POST)
     public Result weixinQueryCustomerAccountLogPage(@RequestBody @Validated InterfaceCustomerAccountLogParam param, BindingResult validResult) {
         boolean erpIdentity = businessSystemConfigService.verifyErpIdentity(param.getErpAppId(), param.getErpAppSecret());
-        if(erpIdentity){
+        if (erpIdentity) {
             ServiceResult<String, Map<String, Object>> serviceResult = paymentService.weixinQueryCustomerAccountLogPage(param);
             return resultGenerator.generate(serviceResult.getErrorCode(), serviceResult.getResult());
         }
@@ -190,11 +203,23 @@ public class InterfaceController extends BaseController {
     @RequestMapping(value = "subCompanyPage", method = RequestMethod.POST)
     public Result pageSubCompany(@RequestBody @Validated InterfaceSubCompanyQueryParam param, BindingResult validResult) {
         boolean erpIdentity = businessSystemConfigService.verifyErpIdentity(param.getErpAppId(), param.getErpAppSecret());
-        if(erpIdentity){
+        if (erpIdentity) {
             ServiceResult<String, Page<SubCompany>> serviceResult = companyService.subCompanyPage(param);
             return resultGenerator.generate(serviceResult.getErrorCode(), serviceResult.getResult());
         }
         return resultGenerator.generate(ErrorCode.BUSINESS_SYSTEM_ERROR);
+    }
+
+    @RequestMapping(value = "addPerson", method = RequestMethod.POST)
+    public Result addPerson(@RequestBody @Validated(AddCustomerPersonGroup.class) Customer customer, BindingResult validResult) {
+        if (userSupport.getCurrentUser() == null) {
+            User superUser = new User();
+            superUser.setUserId(CommonConstant.SUPER_USER_ID);
+            httpSession.setAttribute(CommonConstant.ERP_USER_SESSION_KEY, superUser);
+        }
+        ServiceResult<String, String> serviceResult = customerService.addPerson(customer);
+        httpSession.setAttribute(CommonConstant.ERP_USER_SESSION_KEY, null);
+        return resultGenerator.generate(serviceResult.getErrorCode(), serviceResult.getResult());
     }
 
 }
