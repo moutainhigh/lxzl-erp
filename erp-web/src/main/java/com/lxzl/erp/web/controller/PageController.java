4a56268583bd17f85145f927aa8aa354915e1900
Vincent <515420357@qq.com>
2018-08-02 19:07:50 +0800

    edit

--- erp-web/src/main/java/com/lxzl/erp/web/controller/PageController.java
+++ erp-web/src/main/java/com/lxzl/erp/web/controller/PageController.java
@@ -1224,4 +1224,10 @@ public class PageController extends BaseController {
     public String addSystemNotice() {
         return "/systemManage/addSystemNotice";
     }
+
+    //延时任务列表
+    @RequestMapping("/delayed-task/list")
+    public String deyayedTaskList() {
+        return "/delayedTask/deyayedTaskList";
+    }
 }
