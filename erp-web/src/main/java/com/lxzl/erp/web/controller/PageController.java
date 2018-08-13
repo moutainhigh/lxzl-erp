ec24c3348f8cdebd1475d0e782b9e6fb074233ca
Vincent <515420357@qq.com>
2018-08-13 19:28:11 +0800

    修改添加、编辑系统公告

--- erp-web/src/main/java/com/lxzl/erp/web/controller/PageController.java
+++ erp-web/src/main/java/com/lxzl/erp/web/controller/PageController.java
@@ -1253,6 +1253,12 @@ public class PageController extends BaseController {
         return "/systemManage/addNoticePage";
     }
 
+    //添加系统消息
+    @RequestMapping("/system-manage/notice-edit")
+    public String editSystemNotice() {
+        return "/systemManage/editSystemNotice";
+    }
+
     //定时任务执行者列表
     @RequestMapping("/timed-manage/executor-list")
     public String timerManageList() {
