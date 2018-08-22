282d3bc5d7a4a3dc13c941c899c303c9bdf55dc1
Vincent <515420357@qq.com>
2018-08-22 15:19:20 +0800

    公海流水

--- erp-web/src/main/java/com/lxzl/erp/web/controller/PageController.java
+++ erp-web/src/main/java/com/lxzl/erp/web/controller/PageController.java
@@ -1145,6 +1145,16 @@ public class PageController extends BaseController {
         return "/financialManage/amountClaimDetailList";
     }
 
+    @RequestMapping("/finance-manage/unknown-list")
+    public String financeUnknownList() {
+        return "/financialManage/jurnalUnknownList";
+    }
+
+    @RequestMapping("/finance-manage/unknown-detail")
+    public String financeUnknownDetail() {
+        return "/financialManage/jurnalUnknownDetail";
+    }
+
     /**
      * 系统功能开关设置
      *
