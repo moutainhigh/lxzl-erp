ad4813c793c1627277cdbe2c0fd7bd3c36272cd0
sunzhipeng <250697148@qq.com>
2018-09-29 11:12:08 +0800

    退货单换货单字段添加

--- erp-common/src/main/java/com/lxzl/erp/common/domain/k3/pojo/returnOrder/K3ReturnOrderDetail.java
+++ erp-common/src/main/java/com/lxzl/erp/common/domain/k3/pojo/returnOrder/K3ReturnOrderDetail.java
@@ -41,6 +41,7 @@ public class K3ReturnOrderDetail extends BasePO {
 
     private Date returnTime;  //退货时间
     private Integer orderItemType;//商品类型,1-商品,2-配件
+    private String originalOrderNo;  //原订单单号
 
     public Integer getK3ReturnOrderDetailId() {
         return k3ReturnOrderDetailId;
@@ -200,5 +201,13 @@ public class K3ReturnOrderDetail extends BasePO {
 
     public void setOrderItemType(Integer orderItemType) {
         this.orderItemType = orderItemType;
+    }
+
+    public String getOriginalOrderNo() {
+        return originalOrderNo;
+    }
+
+    public void setOriginalOrderNo(String originalOrderNo) {
+        this.originalOrderNo = originalOrderNo;
     }
 }
